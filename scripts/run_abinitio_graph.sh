ff8dc815fa881fddcb0baa7b118e4f742b63c8e8
sat2594 <sat2594@gmail.com>
2025-06-15 10:18:33 +0530

    develop changed

--- scripts/run_abinitio_graph.sh
+++ scripts/run_abinitio_graph.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Running Ab Initio graph in the name of sam "
+echo "Running Ab Initio graph in the name of samdevelop "
 # Example command to run the graph
 /path/to/abinitio/ab_run.sh /path/to/graph_name.mp
